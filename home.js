1ff71cbb463213bb801a776ce4e8f320e817e98c
Rishikesh_Shandilya <rs0206585@gmail.com>
2025-07-12 12:40:38 +0530

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-// === script.js ===
-
-document.querySelectorAll(".rating-section input").forEach(input => {
-  input.addEventListener("input", function () {
-    const value = parseFloat(this.value);
-    const span = this.nextElementSibling;
-    span.textContent = isNaN(value) ? "/ 5" : `⭐ ${value} / 5`;
-  });
-});
